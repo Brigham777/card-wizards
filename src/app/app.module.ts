2067201cb7ad95245f861abbac8e3f9c26ae62d2
Brigham777 <ighambray@outlook.com>
2019-07-17 22:26:57 -0600

    Added UI components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatListModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MainUIComponent } from './main-ui/main-ui.component';
+import { CardsComponent } from './ui-components/cards/cards.component';
+import { TopNavComponent } from './ui-components/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainUIComponent,
+    CardsComponent,
+    TopNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
